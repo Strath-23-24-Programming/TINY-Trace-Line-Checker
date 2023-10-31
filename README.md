e3e9e685db6340963dc8d4759af1ad53a31ea0c2
Helm <89736173+TurtleHelm@users.noreply.github.com>
2023-10-31 01:47:27 +0000

    main commit

---
+++ README.md
@@ -0,0 +1,105 @@
+# TINYsim
+
+Simulation program for Conor McBride's TINY Machine.
+
+Example - Correct Output
+
+```f
+The following inputs are for the starter trace (What you're given)
+-------------------------------------------------------------------
+Enter input queue (Format: x, x, x, x...): 0, 0, 1, C, 0, 7, C, A
+Enter TINY configuration (Format: x x x x  xxxxxxxxxxxxxxx): 0 0 0 0 65F65E65D65C8F70
+
+The following inputs are for the trace to check
+-----------------------------------------------
+Enter TINY configuration (Format: x x x x  xxxxxxxxxxxxxxx): 3 0 2 0 65F65E65D65C8F70 
+Enter the final piece of the trace (format xxx x x x): GET - 0 -
+I L F A  Memory----------  Action---
+P I R C  0123456789ABCDEF  OPR & ? !
+-------  ----------------  ---------
+3 0 2 0  65F65E65D65C8F70  GET - 0 - <-- YOUR INPUT
+-------  ----------------  ---------
+TRACE INPUT CORRECT <-- TRACE OUTPUT
+Hit Enter to Finish...
+```
+
+Example - Incorrect Registry Config
+
+```f
+The following inputs are for the starter trace (What you're given)
+-------------------------------------------------------------------
+Enter input queue (Format: x, x, x, x...): 0, 0, 1, C, 0, 7, C, A
+Enter TINY configuration (Format: x x x x  xxxxxxxxxxxxxxx): 0 0 0 0 65F65E65D65C8F70
+
+The following inputs are for the trace to check
+-----------------------------------------------
+Enter TINY configuration (Format: x x x x  xxxxxxxxxxxxxxx): 3 0 1 0 65F65E65D65C8F70 
+Enter the final piece of the trace (format xxx x x x): GET - 0 -
+I L F A  Memory----------  Action---
+P I R C  0123456789ABCDEF  OPR & ? !
+-------  ----------------  ---------
+3 0 2 0  65F65E65D65C8F70  GET - 0 - <-- YOUR INPUT
+-------  ----------------  ---------
+ERROR WITH REGISTRY  <-- TRACE OUTPUT
+Hit Enter to Finish...
+```
+
+Example - Incorrect Memory Config
+
+```f
+The following inputs are for the starter trace (What you're given)
+-------------------------------------------------------------------
+Enter input queue (Format: x, x, x, x...): 0, 0, 1, C, 0, 7, C, A
+Enter TINY configuration (Format: x x x x  xxxxxxxxxxxxxxx): 0 0 0 0 65F65E65D65C8F70
+
+The following inputs are for the trace to check
+-----------------------------------------------
+Enter TINY configuration (Format: x x x x  xxxxxxxxxxxxxxx): 3 0 2 0 55F65E65D65C8F70 
+Enter the final piece of the trace (format xxx x x x): GET - 0 -
+I L F A  Memory----------  Action---
+P I R C  0123456789ABCDEF  OPR & ? !
+-------  ----------------  ---------
+3 0 2 0  65F65E65D65C8F70  GET - 0 - <-- YOUR INPUT
+-------  ----------------  ---------
+ERROR WITH MEMORY <-- TRACE OUTPUT
+Hit Enter to Finish...
+```
+
+Example - Incorrect Action Config
+
+```f
+The following inputs are for the starter trace (What you're given)
+-------------------------------------------------------------------
+Enter input queue (Format: x, x, x, x...): 0, 0, 1, C, 0, 7, C, A
+Enter TINY configuration (Format: x x x x  xxxxxxxxxxxxxxx): 0 0 0 0 65F65E65D65C8F70
+
+The following inputs are for the trace to check
+-----------------------------------------------
+Enter TINY configuration (Format: x x x x  xxxxxxxxxxxxxxx): 3 0 2 0 65F65E65D65C8F70 
+Enter the final piece of the trace (format xxx x x x): GET - 1 -
+I L F A  Memory----------  Action---
+P I R C  0123456789ABCDEF  OPR & ? !
+-------  ----------------  ---------
+3 0 2 0  65F65E65D65C8F70  GET - 0 - <-- YOUR INPUT
+-------  ----------------  ---------
+ERROR WITH ACTIONS <-- TRACE OUTPUT
+Hit Enter to Finish...
+```
+
+## How To Use
+
++ Run the script with Python 3 (3.10 tested)
++ Input a TINY input queue in the format "x, x, x, x..."
++ Input a starting TINY configuration in the format "x x x x  xxxxxxxxxxxxxxx"
++ Input a TINY configuration to check for in the format "x x x x  xxxxxxxxxxxxxxx"
++ Input a TINY action configuration to check for in the format "xxx x x x"
++ The program should then print if your TINY config and action are correct, if it is not it should tell you where
+
+## Notes
+
++ This script halts after 500 operations, and assumes that the TINY program loops indefinitely
++ This can be changed by editing the main loop of the program, line 298. Set count to a desired value.
+
+## Known Bugs
+
+### Currently No Known Bugs
---
+++ main.py
@@ -0,0 +1,402 @@
+class Trace:
+	def __init__(self):
+		self.memory = [0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0] # TINY Memory (16 Bit)
+		self.registry = [0x0,0x0,0x0,0x0] # TINY Registry (4 Bit)
+		self.inQueue, self.outQueue, self.output = [], [], []
+		self.action = ['', '', '', '']
+		self.stop = False
+		self.reason = ''
+
+	def incrementIP(self, amt:int) -> None:
+		self.registry[0] += amt
+		if self.registry[0] >= 16: self.registry[0] -= 16
+
+	def setAction(self, newAction:list) -> None:
+		"""Set New Action List
+
+		Args:
+			newAction (list): new action list to append
+		"""
+		for i in range(len(newAction)): self.action[i] = newAction[i]
+
+	@staticmethod
+	def hexToStr(hexIn:int) -> str:
+		"""Convert from Hexadecimal to Strings
+
+		Args:
+			hexIn (int): Hex Code to Take In
+
+		Returns:
+			str: Stringified Hexadecimal
+		"""
+		if hexIn > 9: return str(hex(hexIn))[-1].upper()
+		else: return str(hexIn)
+
+	@staticmethod
+	def strToHex(strIn) -> int:
+		"""Convert From Stringified Hexadecimal to Hex Codes
+
+		Args:
+			strIn (str): String to Convert
+
+		Returns:
+			int: Hex Code
+		"""
+	
+		if strIn == "A": return 10
+		if strIn == "B": return 11
+		if strIn == "C": return 12
+		if strIn == "D": return 13
+		if strIn == "E": return 14
+		if strIn == "F": return 15
+		return int(strIn)
+
+	def printState(self) -> None:
+		"""Prints current state of the TINY Machine"""
+
+		printStr = ''
+
+		for reg in self.registry: printStr += f'{self.hexToStr(reg)} '  
+
+		printStr += ' '
+
+		for mem in self.memory: printStr += self.hexToStr(mem)
+
+		printStr += ' '
+	
+		for action in self.action: printStr += f' {action}'
+		
+		self.output.append(printStr)
+
+class Operators:
+    """Different Operations used in the TINY Machine
+    - HLT: Halt (Stops execution)
+    - JMP: Jump (Jumps to designated address)
+    - JZE: Jump if Zero (Jumps to designated address if zero flag = 0)
+    - JNZ: Jump if not Zero (Jumps to designated address if zero flag != 0)
+    - LDA: Load Accumulator (Loads accumulator from designated address)
+    - STA: Store Accumulator (Store accumulator to designated address)
+    - GET: Get Accumulator (Get accumulator from input queue)
+    - PUT: Put Accumulator (Put accumulator into output queue)
+    - ROL: Rotate Left (Double and add carry-in)
+    - ROR: Rotate Right (Half and carry-out remainder)
+    - ADC: Add With Carry
+    - CCF: Clear Carry Flag
+    - SCF: Set Carry Flag
+    - DEL: Decrement Loop Index
+    - LDL: Load Loop Index (Load loop index from designated address)
+    - FLA: Flip Accumulator
+    """
+    
+    def __init__(self, trace:Trace): self.trace = trace
+    
+    def HLT(self) -> None:
+        self.trace.setAction(['HLT', '-', '-', '-'])
+        self.trace.printState()
+        
+        self.trace.registry[2] = self.trace.registry[2] | 0b1000
+        self.trace.incrementIP(1)
+        
+        self.trace.setAction(["","","",""])
+        self.trace.printState()
+        
+        self.trace.stop = True
+        self.trace.reason = "Halted Normally"
+        
+    def JMP(self) -> None:
+        self.trace.setAction(['JMP', self.trace.hexToStr(self.trace.memory[self.trace.registry[0]+1]), '-', '-'])
+        self.trace.printState()
+        
+        self.trace.registry[0] = self.trace.memory[self.trace.registry[0]+1]
+            
+    def JZE(self) -> None:
+        self.trace.setAction(["JZE", self.trace.hexToStr(self.trace.memory[self.trace.registry[0]+1]),"-","-"])
+        self.trace.printState()
+        
+        if self.trace.registry[2] & 0b10 == 0b10: self.trace.registry[0] = self.trace.memory[self.trace.registry[0]+1]
+        else: self.trace.incrementIP(2)
+        
+    def JNZ(self) -> None:
+        self.trace.setAction(["JNZ", self.trace.hexToStr(self.trace.memory[self.trace.registry[0]+1]),"-","-"])
+        self.trace.printState()
+        
+        if self.trace.registry[2] & 0b10 != 0b10: self.trace.registry[0] = self.trace.memory[self.trace.registry[0]+1]
+        else: self.trace.incrementIP(2)
+        
+    def LDA(self) -> None:
+        self.trace.setAction(["LDA", self.trace.hexToStr(self.trace.memory[self.trace.registry[0]+1]),"-","-"])
+        self.trace.printState()
+        
+        self.trace.registry[3] = self.trace.memory[self.trace.memory[self.trace.registry[0]+1]]
+        
+        if self.trace.registry[3] == 0: self.trace.registry[2] = self.trace.registry[2] | 0b10
+        else: self.trace.registry[2] = self.trace.registry[2] & 0b1101
+        
+        self.trace.incrementIP(2)
+        
+    def STA(self) -> None:
+        self.trace.setAction(["STA", self.trace.hexToStr(self.trace.memory[self.trace.registry[0]+1]),"-","-"])
+        self.trace.printState()
+        
+        self.trace.memory[self.trace.memory[self.trace.registry[0]+1]] = self.trace.registry[3]
+        self.trace.incrementIP(2)
+        
+    def GET(self) -> None:
+        if len(self.trace.inQueue) != 0:
+            self.trace.setAction(["GET","-", self.trace.hexToStr(self.trace.inQueue[0]),"-"])
+            self.trace.printState()
+            
+            self.trace.registry[3] = self.trace.inQueue[0]
+            
+            del self.trace.inQueue[0]
+            
+            if self.trace.registry[3] == 0: self.trace.registry[2] = self.trace.registry[2] | 0b10
+            else: self.trace.registry[2] = self.trace.registry[2] & 0b1101
+            
+        else:
+            self.trace.setAction(["GET","-","-","-"])
+            self.trace.printState()
+            
+            self.trace.stop = True
+            self.trace.reason = "Starved For Input"
+            
+        self.trace.incrementIP(1)
+        
+    def PUT(self) -> None:
+        self.trace.setAction(["PUT","-","-",self.trace.hexToStr(self.trace.registry[3])])
+        self.trace.printState()
+        
+        self.trace.outQueue.append(self.trace.registry[3])
+        self.trace.incrementIP(1)
+        
+    def ROL(self) -> None:
+        self.trace.setAction(["ROL","-","-","-"])
+        self.trace.printState()
+        
+        self.trace.registry[3] = self.trace.registry[3] * 2 + (self.trace.registry[2] & 0b1)
+        
+        if self.trace.registry[3] >= 16:
+            self.trace.registry[3] -= 16
+            self.trace.registry[2] = self.trace.registry[2] | 0b1
+            
+        else: self.trace.registry[2] = self.trace.registry[2] & 0b1110
+        
+        if self.trace.registry[3] & 0b1000 != (self.trace.registry[3] & 0b1000):
+            self.trace.registry[2] = self.trace.registry[2] | 0b100
+        else: self.trace.registry[2] = self.trace.registry[2] & 0b011
+        
+        if self.trace.registry[3] == 0: self.trace.registry[2] = self.trace.registry[2] | 0b10
+        else: self.trace.registry[2] = self.trace.registry[2] & 0b1101
+        
+        self.trace.incrementIP(1)
+        
+    def ROR(self) -> None:
+        self.trace.setAction(["ROR","-","-","-"])
+        self.trace.printState()
+        
+        x = self.trace.registry[3] & 0b1000
+        if self.trace.registry[3] % 2 == 0:
+            self.trace.registry[3]/=2
+            self.trace.registry[2] = self.trace.registry[2] & 0b1110
+        else:
+            self.trace.registry[3] = (self.trace.registry[3]-1) / 2
+            self.trace.registry[2] = self.trace.registry[2] | 0b1
+            
+        if self.trace.registry[3] & 0b1000 != x: self.trace.registry[2] = self.trace.registry[2] | 0b100
+        else: self.trace.registry[2] = self.trace.registry[2] & 0b011
+            
+        if self.trace.registry[3] == 0: self.trace.registry[2] = self.trace.registry[2] | 0b10
+        else: self.trace.registry[2] = self.trace.registry[2] & 0b1101
+        
+        self.trace.incrementIP(1)
+        
+    def ADC(self) -> None:
+        self.trace.setAction(["ADC", self.trace.hexToStr(self.trace.memory[self.trace.registry[0]+1]),"-","-"])
+        self.trace.printState()
+        carry = 0
+        
+        if(self.trace.registry[3] & 0b1) + (self.trace.memory[self.trace.memory[self.trace.registry[0]+1]] & 0b1) + (self.trace.registry[2] & 0b1) >= 2: carry = 1
+        else: carry = 0
+        
+        if(self.trace.registry[3] & 0b10)/2 + (self.trace.memory[self.trace.memory[self.trace.registry[0]+1]] & 0b10)/2 + carry >= 2: carry = 1
+        else: carry = 0
+        
+        if(self.trace.registry[3] & 0b100)/4 + (self.trace.memory[self.trace.memory[self.trace.registry[0]+1]] & 0b100)/4 + carry >= 2: carry = 1
+        else: carry = 0
+        
+        if(self.trace.registry[3] & 0b1000)/8 + (self.trace.memory[self.trace.memory[self.trace.registry[0]+1]] & 0b1000)/8 + carry >= 2: carry2 = 1
+        else: carry2 = 0
+        
+        if carry2 == carry: self.trace.registry[2] = self.trace.registry[2] & 0b1011
+        else: self.trace.registry[2] = self.trace.registry[2] | 0b0100
+        
+        self.trace.registry[3] = self.trace.registry[3] + (self.trace.registry[2] & 0b1) + self.trace.memory[self.trace.memory[self.trace.registry[0]+1]]
+        
+        if self.trace.registry[3] >=16:
+            self.trace.registry[3] -= 16
+            self.trace.registry[2] = self.trace.registry[2] | 0b1
+        
+        else: self.trace.registry[2] = self.trace.registry[2] & 0b1110
+        
+        if self.trace.registry[3] == 0: self.trace.registry[2] = self.trace.registry[2] | 0b10
+        else: self.trace.registry[2] = self.trace.registry[2] & 0b1101
+        
+        self.trace.incrementIP(2)
+        
+    def CCF(self) -> None:
+        self.trace.setAction(["CCF","-","-","-"])
+        self.trace.printState()
+        
+        self.trace.registry[2] = self.trace.registry[2] & 0b1110
+        self.trace.incrementIP(1)
+        
+    def SCF(self) -> None:
+        self.trace.setAction(["SCF","-","-","-"])
+        self.trace.printState()
+        
+        self.trace.registry[2] = self.trace.registry[2] | 0b0001
+        self.trace.incrementIP(1)
+        
+    def DEL(self) -> None:
+        self.trace.setAction(["DEL","-","-","-"])
+        self.trace.printState()
+        
+        self.trace.registry[1]-=1
+        
+        if self.trace.registry[1]<0: self.trace.registry[1]+=16
+        
+        if self.trace.registry[1] == 0: self.trace.registry[2] = self.trace.registry[2] | 0b0010
+        else: self.trace.registry[2] = self.trace.registry[2] & 0b1101
+        
+        self.trace.incrementIP(1)
+        
+    def LDL(self) -> None:
+        self.trace.setAction(["LDL", self.trace.hexToStr(self.trace.memory[self.trace.registry[0]+1]),"-","-"])
+        self.trace.printState()
+        
+        self.trace.registry[1] = self.trace.memory[self.trace.memory[self.trace.registry[0]+1]]
+        if self.trace.registry[1] == 0: self.trace.registry[2] = self.trace.registry[2] | 0b0010
+        else: self.trace.registry[2] = self.trace.registry[2] & 0b1101
+        
+        self.trace.incrementIP(2)
+        
+    def FLA(self) -> None:
+        self.trace.setAction(["FLA","-","-","-"])
+        self.trace.printState()
+        
+        self.trace.registry[3] = 15 - self.trace.registry[3]
+        
+        if self.trace.registry[3] < 0: self.trace.registry[3]+=1
+        if self.trace.registry[3] == 0: self.trace.registry[2] = self.trace.registry[2] | 0b0010
+        else: self.trace.registry[2] = self.trace.registry[2] & 0b1101
+        
+        self.trace.incrementIP(1)
+
+def makeInputLine(mem, actions:str) -> str:
+    reg, out = '', ''
+
+    for pos in mem[0:4]: reg += pos + ' '
+    
+    mem = mem[4:]
+    
+    for pos in reg.split(' '): out += str(pos) + ' '
+    for pos in mem.split(' '): out += str(pos)
+        
+    out += ' '
+    
+    for act in actions.split(' '): out += ' ' + act
+
+    return out
+
+def checkTraceOutput(output:list, traceInput:str) -> str:        
+    regLineCheck, memLineCheck, actLineCheck = False, False, False
+
+    for line in output:
+        if line.split('  ')[0] == traceInput.split('  ')[0]: regLineCheck = True
+        
+        if regLineCheck:
+            if line.split('  ')[1] == traceInput.split('  ')[1]: memLineCheck = True
+        
+        if memLineCheck:
+            if line.split('  ')[2] == traceInput.split('  ')[2]: actLineCheck = True
+        
+        if actLineCheck:
+            if line == traceInput: return 'TRACE INPUT CORRECT'
+        
+    if regLineCheck != True: return 'ERROR WITH REGISTRY'
+    elif memLineCheck != True: return 'ERROR WITH MEMORY'
+    else: return 'ERROR WITH ACTIONS'
+
+trace = Trace()
+    
+def main(tinyTrace:Trace) -> None:
+    
+    ops = Operators(tinyTrace)
+    
+    print('The following inputs are for the starter trace (What you\'re given)')
+    print('-------------------------------------------------------------------')
+    
+    inputString = input('Enter input queue (Format: x, x, x, x...): ').replace(', ','')
+    for char in inputString: tinyTrace.inQueue.append(tinyTrace.strToHex(char))
+    
+    inputString = input('Enter TINY configuration (Format: x x x x  xxxxxxxxxxxxxxx): ').replace(' ', '')
+    
+    # Load data into arrays
+    for i, char in enumerate(inputString):
+        if i < 20:
+            if i < 4: tinyTrace.registry[i] = tinyTrace.strToHex(char)
+            else: tinyTrace.memory[i-4] = tinyTrace.strToHex(char)
+            
+        i += 1
+        
+    print('\nThe following inputs are for the trace to check')
+    print('-----------------------------------------------')
+
+    inpTrace = input('Enter TINY configuration (Format: x x x x  xxxxxxxxxxxxxxx): ').replace(' ', '')
+    inpTraceActions = input('Enter the final piece of the trace (format xxx x x x): ')
+    
+    inputLine = makeInputLine(inpTrace, inpTraceActions)
+    
+    # Trace Header
+    print('I L F A  Memory----------  Action---')
+    print('P I R C  0123456789ABCDEF  OPR & ? !')
+    print('-------  ----------------  ---------')
+    print(f'{inputLine} <-- YOUR INPUT')
+    
+    global count
+    count = 0 # Iterations Through Trace
+    
+    # Apply Operators to Traces
+    while not tinyTrace.stop:
+        if count >= 500:
+            trace.stop = True
+            trace.reason = "Loops Henceforth"
+            
+        match tinyTrace.memory[tinyTrace.registry[0]]:
+            case 0: ops.HLT()
+            case 1: ops.JMP()
+            case 2: ops.JZE()
+            case 3: ops.JNZ()
+            case 4: ops.LDA()
+            case 5: ops.STA()
+            case 6: ops.GET()
+            case 7: ops.PUT()
+            case 8: ops.ROL()
+            case 9: ops.ROR()
+            case 10: ops.ADC()
+            case 11: ops.CCF()
+            case 12: ops.SCF()
+            case 13: ops.DEL()
+            case 14: ops.LDL()
+            case 15: ops.FLA()
+            case _: print('Error: Exceeded TINY Operator List')
+            
+        count += 1
+
+    print('-------  ----------------  ---------')
+    print(f'{checkTraceOutput(trace.output, inputLine)} <-- TRACE OUTPUT')
+    print('-------  ----------------  ---------')
+    
+    input('Hit Enter to Finish...')
+
+main(trace)
